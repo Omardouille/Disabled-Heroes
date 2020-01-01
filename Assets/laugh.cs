2cc1aa0fff35fcd1c9182c99422c7c3f66b28c71
Dosseum <elliotgaud@hotmail.fr>
2020-01-01 23:59:17 +0100

    Niveau aveugle + son fmod

---
+++ Assets/laugh.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class laugh : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            GetComponent<FMODUnity.StudioEventEmitter>().Play();
+            Debug.Log("test");
+        }
+    }
+
+    /*
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+            GetComponent<FMODUnity.StudioEventEmitter>().Stop();
+    }
+    */
+}
