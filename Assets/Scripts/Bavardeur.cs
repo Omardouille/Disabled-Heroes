2a5453f63e6f0a8b9ccab6d16505bc6ec0b51998
Manel Fennira <47716612+moftian@users.noreply.github.com>
2020-01-03 08:47:18 +0100

    Scene

---
+++ Assets/Scripts/Bavardeur.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bavardeur : MonoBehaviour
+{
+    public List<string> rumeures;
+    public GameObject msgBox;
+    public Vector2 offset;
+
+    private Transform owner;
+
+    private void Start()
+    {
+        owner = gameObject.GetComponentInParent<Transform>();
+        Debug.Log("Owner position : " + owner.position);
+    }
+
+    private void Update()
+    {
+        if (msgBox)
+        {
+            msgBox.transform.position = Camera.main.WorldToScreenPoint(owner.position) + new Vector3(offset.x, offset.y, 0) ;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {            
+            Text text = msgBox.GetComponentInChildren<Text>();
+            text.text = rumeures[Random.Range(0, rumeures.Count)];
+            StartCoroutine("wait");
+        }
+    }
+
+    IEnumerable wait()
+    {
+        yield return new WaitForSeconds(1.0f);
+    }
+}
---
+++ Assets/Scripts/GirlMovement.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GirlMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    public GameObject msgBox;
+
+    private bool isReadyToWalk;
+    private Rigidbody2D rb; 
+    private int state;
+    private int direction;
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        animator = gameObject.GetComponent<Animator>();
+        isReadyToWalk = true;
+        // 0 - idle, 1 - walk
+        state = 0;
+        direction = -1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isReadyToWalk)
+        {
+            state = newState();
+
+            switch (state)
+            {
+                // idle
+                case 0:
+                    rb.velocity = new Vector2(0, 0);
+                    animator.SetInteger("state", 0);
+                    break;
+
+                // move
+                case 1:
+                    if (direction > 0)
+                        animator.SetFloat("moveDirection", 1.0f);
+                    if (direction < 0)
+                        animator.SetFloat("moveDirection", 0.0f);
+                    rb.velocity = new Vector2(direction * moveSpeed, 0);
+                    animator.SetInteger("state", 1);
+                    break;
+
+                default:
+                    break;
+            }
+
+            // Start movement
+            StartCoroutine("walking");
+        }
+    }
+
+    IEnumerator walking()
+    {
+        isReadyToWalk = false;
+        yield return new WaitForSeconds(2.0f);
+        isReadyToWalk = true;
+        direction *= -1;
+    }
+
+    int newState()
+    {
+        return Random.Range(0, 2);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            msgBox.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            msgBox.SetActive(false);
+        }
+    }
+}
