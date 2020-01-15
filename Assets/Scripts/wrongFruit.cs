775ba3d3550e8bdfa530621d624ead33149ad42c
Benjamin <simpson.17@hotmail.fr>
2020-01-15 18:17:08 +0100

    script

---
+++ Assets/Scripts/wrongFruit.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wrongFruit : MonoBehaviour
+{
+    GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            gameObject.SetActive(false);
+            player.GetComponent<DHMarchalController>().nbvie--;
+        }
+    }
+}
