d7510981c18553a380ebe8247addda09411cc4ef
Dosseum <elliotgaud@hotmail.fr>
2020-01-15 18:31:21 +0100

    Git POULE

--- Assets/Scripts/VisionBack.cs
+++ Assets/Scripts/VisionBack.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class VisionBack : MonoBehaviour
 {
+
+	private GameObject finLvl;
     // Start is called before the first frame update
     void Start()
     {
-        
+        finLvl = GameObject.Find("Sortie");
+		finLvl.SetActive(false);
     }
 
     // Update is called once per frame
@@ -20,7 +23,7 @@ public class VisionBack : MonoBehaviour
 		if(c.gameObject.tag == "Player"){
 			// On autorise la porte
 			GameObject.Find("Main Camera").GetComponents<ColorBlindFilter>()[0].mode = ColorBlindMode.Normal;
-			
+			finLvl.SetActive(true);
 			gameObject.SetActive(false);
 		}
 	}
